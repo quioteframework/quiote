81e720ae4b14ddcd5c61a6eae9e22aad7837370c
Markus Lervik <markus.lervik@thejakamo.com>
2025-05-21 15:10:26 +0300

    Don't urlencode null values

--- src/response/AgaviWebResponse.class.php
+++ src/response/AgaviWebResponse.class.php
@@ -708,7 +708,8 @@ class AgaviWebResponse extends AgaviResponse
 			}
 
 			if($values['encode_callback']) {
-				$values['value'] = $values['encode_callback'] != null ? call_user_func($values['encode_callback'], $values['value']) : $values['value'];
+				if ($values['value'] !== null)
+					$values['value'] = call_user_func($values['encode_callback'], $values['value']);
 			}
 			
 			if($values['path'] === null) {
