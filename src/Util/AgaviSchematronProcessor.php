34362f78405901409ea33e7d3094711fa25d4443
Markus Lervik <markus.lervik@thejakamo.com>
2025-05-25 22:06:40 +0300

    Fix NS

--- src/Util/AgaviSchematronProcessor.php
+++ src/Util/AgaviSchematronProcessor.php
@@ -80,7 +80,7 @@ class AgaviSchematronProcessor extends AgaviParameterHolder
 			throw new AgaviException('Schematron processor chain must contain at least one path name.');
 		}
 		
-		$this->chain = array_map(['AgaviToolkit', 'expandDirectives'], $chain);
+		$this->chain = array_map(['Agavi\\Util\\AgaviToolkit', 'expandDirectives'], $chain);
 	}
 	
 	/**
