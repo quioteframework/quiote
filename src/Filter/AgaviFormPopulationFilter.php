152c0f8a22c2c964a4b8b533764a4e051da00461
Markus Lervik <markus.lervik@thejakamo.com>
2025-07-10 08:10:47 +0300

    AgaviFormPopulationFilter is a post-filter, fix PHP 8.4 compat

--- src/Filter/AgaviFormPopulationFilter.php
+++ src/Filter/AgaviFormPopulationFilter.php
@@ -262,9 +262,7 @@ class AgaviFormPopulationFilter extends AgaviFilter implements AgaviIGlobalFilte
 		}
 
 		if(($encoding = $cfg['force_encoding']) === false) {
-			if($this->doc->actualEncoding) {
-				$encoding = $this->doc->actualEncoding;
-			} elseif($this->doc->encoding) {
+			if($this->doc->encoding) { // doc->actualEncoding is deprecated in PHP 8.4
 				$encoding = $this->doc->encoding;
 			} else {
 				$encoding = $this->doc->encoding = self::ENCODING_UTF_8;
@@ -724,7 +722,7 @@ class AgaviFormPopulationFilter extends AgaviFilter implements AgaviIGlobalFilte
 
 	public function isPostFilter(): bool
 	{
-		return false;
+		return true;
 	}
 
 	/**
